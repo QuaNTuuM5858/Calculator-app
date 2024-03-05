3a0255c3341e089d44e982a68ced74b653839716
QuaNTuuM5858 <aladib12@students.amikom.ac.id>
2024-03-05 12:17:32 +0700

    Menambahkan main

--- Calculator.cs
+++ Calculator.cs
@@ -2,11 +2,22 @@ class calculaotr
 {
 	static void main(string[] args}
 	{
-	
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan : {0} + {1}", a, b, Penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan : {0} + {1}", a, b, Pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan Apa Saja")
+		Console.WriteLine();
 	}
 	static int Penambahan{int a, int b}
 	{
 		return a + b;
+	}
+	static int Pengurangan{int a, int b}
+	{
+		return a - b;
 	}
 {
 	
